574b91089be33c7941ceda9f6abe931766aea8d7
Emma Northridge <emmanorthridge1@gmail.com>
2020-11-16 17:08:27 +0100

    Added Log in functionality

--- clean-closet/routes/auth.js
+++ clean-closet/routes/auth.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const router = new Router();
 const User = require("../models/users.model");
-const Post = require("../models/post.model")
+const Post = require("../models/post.model");
 const bcryptjs = require("bcryptjs");
 
 router.get("/signup", (req, res) => res.render("auth/signup"));
@@ -21,12 +21,10 @@ router.post("/signup", (req, res, next) => {
 
   const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
   if (!regex.test(password)) {
-    res
-      .status(500)
-      .render("auth/signup", {
-        errorMessage:
-          "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",
-      });
+    res.status(500).render("auth/signup", {
+      errorMessage:
+        "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",
+    });
     return;
   }
 
@@ -35,11 +33,11 @@ router.post("/signup", (req, res, next) => {
     .then((salt) => bcryptjs.hash(password, salt))
     .then((hashedPassword) => {
       return User.create({
-          username,
-          name,
-          lastName,
-          country,
-          password: hashedPassword,
+        username,
+        name,
+        lastName,
+        country,
+        password: hashedPassword,
       });
     })
     .then((userFromDB) => {
@@ -58,7 +56,34 @@ router.post("/signup", (req, res, next) => {
     });
 });
 
-router.get("/userProfile", (req, res) => 
-    res.render("auth/profile")); 
+router.get("/login", (req, res) => res.render("auth/login"));
 
-    module.exports = router;
+router.post("/login", (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.render("auth/login", {
+      errorMessage:
+        "All fields are mandatory. Please provide your username and password.",
+    });
+    return;
+  }
+
+  User.findOne({ username })
+    .then((user) => {
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage: "Username is not registered. Try with other username.",
+        });
+        return;
+      } else if (bcryptjs.compareSync(password, user.password)) {
+        res.redirect("/userProfile");
+      } else {
+        res.render("auth/login", { errorMessage: "Incorrect password." });
+      }
+    })
+    .catch((error) => next(error));
+});
+
+router.get("/userProfile", (req, res) => res.render("auth/profile"));
+
+module.exports = router;
