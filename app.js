1da9fef8b193cf7c1afc7bf931eed53316cf61f0
Emma Northridge <emmanorthridge1@gmail.com>
2020-11-19 11:54:40 +0100

    config

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 const app = express();
 
 
-require('./configs/db.config');
+const { connectDB } = require('./configs/db.config');
+connectDB();
 require('./configs/session.configs')(app);
 
 
